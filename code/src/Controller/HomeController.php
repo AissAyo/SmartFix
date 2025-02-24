3da498b720c298551e9ea307f3b93b0feac1ac77
Ayoub <ayoubaissaoui19.03@gmail.com>
2025-02-24 10:04:06 +0000

    WIP: Saving progress before pulling changes

--- code/src/Controller/HomeController.php
+++ code/src/Controller/HomeController.php
@@ -2,11 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\component\Httpfoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
@@ -17,5 +15,4 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-
 }
