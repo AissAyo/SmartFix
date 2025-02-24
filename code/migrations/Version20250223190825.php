88bdae23fe8486224159c57525335560a455a230
HoussamKhadraoui <houkhadraoui@gmail.com>
2025-02-24 10:09:16 +0000

    last com in progress

--- code/migrations/Version20250223190825.php
+++ code/migrations/Version20250223190825.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250216182026 extends AbstractMigration
+final class Version20250223190825 extends AbstractMigration
 {
     public function getDescription(): string
     {
