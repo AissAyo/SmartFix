04208c44954c07c39b470121f972ac2aeb4160e6
Ayoub <ayoubaissaoui19.03@gmail.com>
2025-03-02 14:06:17 +0000

    resolved

--- code/assets/js/app.js
+++ code/assets/js/app.js
@@ -5,3 +5,8 @@ import './styles/css/protfolio.css';
 import './styles/css/slidbar.css';
 
 // Ajoute ici d'autres importations ou logiques JavaScript
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProfilePage from './js/ProfilePage';
+
+ReactDOM.render(<ProfilePage />, document.getElementById('root'));
