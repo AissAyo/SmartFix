64af92bff664ee9d20689c42f6bbd4dff3826696
ooumniaa <oumniaoumnia1111@.com>
2025-03-07 01:12:34 +0000

    version 2 layout admin

--- code/assets/dashboard/dashboard.js
+++ code/assets/dashboard/dashboard.js
@@ -2,7 +2,7 @@ import './js/AdminLTE.js';
 import './js/Layout.js';
 import './js/NavbarSearch.js';
 import './js/Fullscreen.js';
-import './dashboard/dashboard.css';
+import './dashboard.css';
 import './js/SidebarSearch.js';
 import './js/DirectChat.js';
 
