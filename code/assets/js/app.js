360c4d39fb273b4444758af3b110e55e877763fb
HoussamKhadraoui <houkhadraoui@gmail.com>
2025-04-18 23:09:39 +0100

    Webpack config && Stabilized Assets

--- code/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-// assets/js/app.js
-
-
-
-// Ajoute ici d'autres importations ou logiques JavaScript
--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,31 +1,123 @@
 const Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 Encore
-    // Set the output path for compiled assets
+    // Output and public paths
     .setOutputPath('public/build/')
     .setPublicPath('/build')
 
-    // Enable source maps for easier debugging
+    // Enable source maps in dev
     .enableSourceMaps(!Encore.isProduction())
 
-    // Clean the output directory before each build
+    // Clean before build
     .cleanupOutputBeforeBuild()
 
-    // Enable versioning (hashing filenames for cache busting)
+    // Enable versioning in production (you can disable this if you don't need versioning)
     .enableVersioning(Encore.isProduction())
 
-    // Add CSS handling via the CSS loader and style loader
-    .addEntry('app', './assets/app.js')  // Entry point for your JS
-
-    // Enable the CSS loader automatically when you import CSS into your JavaScript
-    .enablePostCssLoader()
-
-    // Enable a single runtime chunk for better caching
+    // Runtime chunk
     .enableSingleRuntimeChunk()
 
-    // Add CSS files (app.css and main.css)
-    .addStyleEntry('app-css', './assets/app.css')
+    // Entry points
+    .addEntry('app', './assets/app.js')
     .addStyleEntry('main-css', './assets/css/main.css')
-;
+    .addStyleEntry('app-css', './assets/app.css')
 
-module.exports = Encore.getWebpackConfig();
+    // Enable PostCSS
+    .enablePostCssLoader()
+
+    // Asset handling for images and fonts (no hashing)
+
+
+    // Copy static assets (including Bootstrap Icons fonts)
+    .addPlugin(new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: './node_modules/jquery-ui-dist/images',
+                to: 'images/[name][ext]'
+            },
+            {
+                from: './node_modules/bootstrap-icons/font/fonts',
+                to: 'fonts/[name][ext]', // No hash in font filenames
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/services',
+                to: 'img/services/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/background',
+                to: 'img/background/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/blog',
+                to: 'img/blog/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/client',
+                to: 'img/client/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/landing',
+                to: 'img/landing/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/more',
+                to: 'img/more/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/shop',
+                to: 'img/shop/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/slider',
+                to: 'img/slider/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/team',
+                to: 'img/team/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/testimonial',
+                to: 'img/testimonial/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img/clients',
+                to: 'img/clients/[name][ext]',
+                noErrorOnMissing: true
+            },
+            {
+                from: './assets/img',
+                to: 'img/[name][ext]',
+                noErrorOnMissing: true
+            }
+
+            // {
+            //     from: './assets/images/',
+            //     to: 'images/[name][ext]',
+            //     noErrorOnMissing: true
+            // }
+        ]
+    }));
+
+const config = Encore.getWebpackConfig();
+
+// Aliases for easier import handling
+config.resolve.alias = {
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js',
+    '../img': path.resolve(__dirname, 'assets/img'),
+    '../images': path.resolve(__dirname, 'assets/images')
+};
+
+module.exports = config;
