3f1ee230d6c588bb3632a448919acc5c6fc9facc
HoussamKhadraoui <houkhadraoui@gmail.com>
2025-04-19 19:21:20 +0100

    service.yaml

--- code/config/bundles.php
+++ code/config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
